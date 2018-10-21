b779cdd845a2040a290ceee5207da2441694ce7f
Parag <parag.raut@outlook.com>
2018-10-21 16:27:13 +0200

    optimisation

--- chimein/urls.py
+++ chimein/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from products.views import home_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 from accounts import urls
 from products import urls
@@ -25,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
